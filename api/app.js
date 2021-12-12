bccd451ce6d6a9bb8f4ef1a9713bb7d83517e84b
Arie <57365299+jayzpkz@users.noreply.github.com>
2021-12-12 23:06:24 +0200

    deleted dropzone import

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 // With middleware
-app.use('/', function(req, res, next){
-    res.send({"name":"victor"});
-    next();
- });
+app.use('/', function (req, res, next) {
+  res.send({ "name": "victor" });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server running successfully on ${PORT}`);
