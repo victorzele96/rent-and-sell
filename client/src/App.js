92031dc0436bf3c672ee16bf56fa86af75133469
Arie <57365299+jayzpkz@users.noreply.github.com>
2021-12-12 23:29:30 +0200

    extracting copyright from signin signup and newproperty into new component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Landpage from './pages/Landpage';
+import Copyright from './components/Copyright';
 
 const App = () => {
   const [toggleMapList, setToggleMapList] = useState(true);
   return (
     <>
-      <Navbar mapList={toggleMapList} setMapList={setToggleMapList}/>
-      <Landpage mapList={toggleMapList}/>
+      <Navbar mapList={toggleMapList} setMapList={setToggleMapList} />
+      <Landpage mapList={toggleMapList} />
+      <Copyright sx={{ mt: 5 }} />
     </>
   );
 };
