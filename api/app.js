96ca10a5f5a71937569cb6759fc4b0ac7d9d8773
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2021-12-13 16:46:32 +0200

    add some routes to node

--- api/app.js
+++ api/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const propertyRoutes = require("./routes/property-routes");
+const userRoutes = require("./routes/user-routes");
+
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 // With middleware
-app.use('/', function(req, res, next){
-    res.send({"name":"victor"});
-    next();
- });
+app.use(userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running successfully on ${PORT}`);
