d6677364bd437bad14ce3f6043a6f21de1d8127e
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2022-03-26 16:31:08 +0300

    fix some css issue in index that effect on map scroll

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
         <Route exact path={"/chats"} element={<Chats />} />
         <Route exact path={"/add-property"} element={<NewProperty />} />
       </Routes>
-      <Copyright sx={{ mt: 4 }} />
+      <Copyright sx={{ mt: 2, md: 2 }} />
     </>
   );
 };
