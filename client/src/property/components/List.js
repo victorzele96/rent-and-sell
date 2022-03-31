14a87eeb65028bd3e3275c26551294ea818ba06d
Arie <57365299+jayzpkz@users.noreply.github.com>
2022-03-31 06:19:35 +0300

    propertyItem expand style fix + on favorites click color

--- client/src/property/components/List.js
+++ client/src/property/components/List.js
@@ -1,14 +1,34 @@
-import { Stack } from "@mui/material";
+import { Container, Stack } from "@mui/material";
+
+import { makeStyles } from '@mui/styles';
 
 import PropertyItem from "./PropertyItem";
 
+const useStyles = makeStyles((theme) => ({
+  listContainer: {
+    "overflow-y": "scroll",
+    marginTop: "40px",
+    maxHeight: "585px",
+  },
+  listContainerInner: {
+    alignItems: "center",
+    paddingBottom: "40px",
+    marginTop: "40px",
+    "overflow": "hidden"
+  }
+}));
+
 const List = (props) => {
+  const classes = useStyles();
+
   return (
-    <Stack spacing="30px" sx={{ alignItems: "center", pb: 5, mt: 5 }}>
-      {props.properties.map(item => (
-        <PropertyItem key={item.id} property={item} />
-      ))}
-    </Stack>
+    <Container maxWidth={false} className={classes.listContainer}>
+      <Stack spacing="30px" className={classes.listContainerInner}>
+        {props.properties.map(item => (
+          <PropertyItem key={item.id} property={item} />
+        ))}
+      </Stack>
+    </Container>
   );
 };
 
