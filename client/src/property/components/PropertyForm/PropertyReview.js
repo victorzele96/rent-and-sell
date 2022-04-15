0c021e5889a2b740914271d0379603a2bbc73196
Arie <57365299+jayzpkz@users.noreply.github.com>
2022-04-15 19:11:52 +0300

    2 background images added to static/images folder + merging session data in PropertyPreview.js

--- client/src/property/components/PropertyForm/PropertyReview.js
+++ client/src/property/components/PropertyForm/PropertyReview.js
@@ -1,23 +1,32 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import PropertyItem from "../PropertyItem"
 
 const PropertyReview = () => {
-  const [item, setItem] = useState({});
+  const [propertyData, setPropertyData] = useState({});
 
-  // useEffect(() => {
-  //   return () => {
-  //     setItem({
-  //       ...JSON.parse(window.sessionStorage.getItem("new-property-state"))
-  //     });
-  //     let images = JSON.parse(window.sessionStorage.getItem("new-property-images"));
-  //     setItem(prevState => [...prevState, images])
-  //     console.log(item);
-  //   }
-  // }, [item]);
+  useEffect(() => {
+    const items = {
+      ...JSON.parse(sessionStorage.getItem("new-property-state")),
+      images: [
+        ...JSON.parse(sessionStorage.getItem("new-property-images"))
+      ]
+    };
+    if (items) {
+      setPropertyData(items);
+    }
+  }, []);
+
+  console.log(propertyData);
 
   return (
     <>
-      {/* <PropertyItem item={item} /> */}
+      <PropertyItem property={propertyData} />
+      {/* {propertyData && Object.keys(propertyData).legnth !== 0 &&
+        <PropertyItem property={propertyData} />
+      }
+      {
+        propertyData && Object.keys(propertyData).legnth === 0 && <p>No data</p>
+      } */}
     </>
   );
 };
