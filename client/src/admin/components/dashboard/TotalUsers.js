7a605218fc4c92903773936b7653dd2c6d82eb4f
Arie <57365299+jayzpkz@users.noreply.github.com>
2022-05-30 14:00:32 +0300

    deleted unused code

--- client/src/admin/components/dashboard/TotalUsers.js
+++ client/src/admin/components/dashboard/TotalUsers.js
@@ -1,5 +1,4 @@
-import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { Avatar, Card, CardContent, Grid, Typography } from '@mui/material';
 import PeopleIcon from '@mui/icons-material/PeopleOutlined';
 
 export const TotalUsers = (props) => (
--- client/src/admin/components/user/UserListToolbar.js
+++ client/src/admin/components/user/UserListToolbar.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import {
   Box,
-  Button,
   Card,
   CardContent,
   TextField,
